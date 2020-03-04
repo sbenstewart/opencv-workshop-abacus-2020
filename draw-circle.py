f2d391f78334bebb93ab9c6e1b84ee348be6eb2d
sbenstewart <sbenstewart@gmail.com>
2020-03-04 06:51:01 +0530

    basics of opencv

---
+++ draw-circle.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+# Step 1. Define callback function
+drawing = False
+ix = -1
+iy = -1
+
+def draw_circle(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+            cv2.circle(img, center = (x, y), radius = 25, 
+                       color = (87, 184, 237), thickness = -1)
+    elif event == cv2.EVENT_RBUTTONDOWN:        
+            cv2.circle(img, center = (x, y), radius = 10,  
+                       color = (87, 184, 237), thickness = 1)
+        
+    
+# Step 2. Call the window
+img = cv2.imread('img/map.png')
+
+cv2.namedWindow(winname = 'my_drawing')
+cv2.setMouseCallback('my_drawing', draw_circle)
+
+
+# Step 3. Execution 
+while True: 
+    cv2.imshow('my_drawing', img)
+    if cv2.waitKey(10) & 0xFF == 27:
+        break
+
+cv2.destroyAllWindows()
